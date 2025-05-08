e86f8accdaf473915a2a765b2137a12b53b5ecb5
[sujinpsujinp] <sujinp038@naver.com>
2025-05-08 10:52:37 +0900

    0508 2nd

---
+++ src/main/finalreact/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import mainimg from './3.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={mainimg}
+      style={{width:'400px'}}/>
+      <h2><b>React+Springboot 배포연습!!</b></h2>
+    </div>
+  );
+}
+
+export default App;
